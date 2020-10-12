ab9e4bee3823e90a934cd7586a152ccd88238a26
Anil Kaundal <60393858+anilkaundal@users.noreply.github.com>
2020-10-12 10:50:57 +0530

    Game of Maximization

---
+++ game_of_maximization.py
@@ -0,0 +1,16 @@
+﻿#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+def maximumStones(arr):
+    evnsum = 0
+    odsum = 0
+    for i in range(len(arr)):
+        if i % 2 == 0:
+            evnsum = evnsum + arr[i]
+        else:
+            odsum = odsum + arr[i]
+    if evnsum >= odsum:
+        return 2 * odsum
+    else:
+        return 2 * evnsum
