695f6a3d16b897f4206bc55a3225aa6820f72c0d
Anil Kaundal <60393858+anilkaundal@users.noreply.github.com>
2020-10-12 10:26:28 +0530

    Cyclic Binary String

---
+++ cyclic_binary_string.py
@@ -0,0 +1,34 @@
+﻿#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+def get_pow(k):
+    count = 0
+    m = 0
+    for i in range(len(k)):
+        if k[i] == '0':
+            count = count + 1
+        else:
+            if m < count:
+                m = count
+            count = 0
+    if m < count:
+        m = count
+    return m
+
+def maximumPower(s):
+    w = 0
+    flag = 0
+    n = len(s)
+    k = list(s)
+    for i in range(n):
+        t = k.pop()
+        k.insert(0, t)
+        if k[0] == '1':
+            w = get_pow(k)
+            break
+        flag = flag + 1
+    if flag == n:
+        return -1
+    else:
+        return w
