b0e302e6e74634f12d89f638ccb534d5bae7817e
Anil Kaundal <60393858+anilkaundal@users.noreply.github.com>
2020-10-15 22:41:29 +0530

    Strictly Increasing Sequence

---
+++ strictly_increasing_sequence.py
@@ -0,0 +1,23 @@
+﻿#!/usr/bin/python
+# -*- coding: utf-8 -*-
+str = ['First', 'Second']
+
+
+def whoIsTheWinner(arr):
+    length = len(arr)
+    nondup = 0
+    dup = 0
+    d = {}
+    for i in range(length):
+        t = arr.pop()
+        if t in d:
+            d[t] = d[t] + 1
+            dup = dup + 1
+        else:
+            d[t] = 1
+            nondup = nondup + 1
+    if dup == 0:
+        return str[0]
+    else:
+        k = nondup + dup - 1
+        return str[k % 2]
